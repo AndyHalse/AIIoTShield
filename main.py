c2f0e8cb3bf432f1ca98dc7869dc163cdcda4e8c
AndyHalse <128831696+AndyHalse@users.noreply.github.com>
2023-04-25 18:10:28 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,28 +1,16 @@
-import tkinter as tk
+from device_detection import DeviceDetector
 from gui import Ui_IoTShield
-from device_detector import DeviceDetector
-from device_clustering import DeviceClustering
+import tkinter as tk
+
 
 class MainWindow:
     def __init__(self):
         self.root = tk.Tk()
-        self.root.title("AI Cyber IoT Shield")
-        self.network = DeviceDetector(user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299')
+        self.network = DeviceDetector(timeout=1, num_threads=100) # add timeout and num_threads here
         self.ui = Ui_IoTShield(self.root, self, self.network)
+        self.ui.create_widgets()
+        self.root.mainloop()
 
-    def scan_devices(self):
-        print("Scanning devices...")
-        devices = self.network.scan_devices()
-        device_clustering = DeviceClustering(devices)
-        clusters = device_clustering.cluster_devices()
-
-        # Update the device list in the GUI with the scanned devices.
-        # You need to implement this function in the Ui_IoTShield class.
-        self.ui.update_device_list(clusters)
-
-    def on_close(self):
-        print("Closing application...")
-        self.root.destroy()
 
 if __name__ == "__main__":
     main_window = MainWindow()
