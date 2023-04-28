04cc6b3bf4ae6caa788e544fb79007a86b91746d
AndyHalse <128831696+AndyHalse@users.noreply.github.com>
2023-04-28 17:36:27 +0200

    Add files via upload
    We have a number of issues to resolve update

--- device_detection.py
+++ device_detection.py
@@ -1,9 +1,8 @@
+import errno
 import ipaddress
+import json
 import logging
-import platform
-import re
 import socket
-import subprocess
 import threading
 import time
 import tkinter as tk
@@ -13,10 +12,9 @@ from concurrent.futures import ThreadPoolExecutor
 from queue import Queue
 from threading import Thread
 from tkinter import messagebox, ttk
-import json
+
 import netifaces
 import nmap
-import psutil
 import requests
 from getmac import get_mac_address
 
@@ -29,52 +27,156 @@ formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-class DeviceDetector:
-    def __init__(self, parent, num_threads=10):
-        super().__init__(parent)
+class CustomDeviceDetector:
+    def __init__(self, parent, timeout_value=3, num_threads=5):
+        self.parent = parent
+        self.timeout_value = timeout_value
         self.num_threads = num_threads
-        self.devices_table = None
-        self.progress_bar = None
-        self.devices = []  # Add this line
+        self.devices = []
+        self.scan_button = None
+        self.running = False
+        self.device_name_entry = None
+        self.device_ip_label = None
+        self.device_ip_entry = None
+        self.add_button = None
+        self.initialize()
+        self.ip_entry = tk.Entry(self)
 
-        # Initialize UI
-        self.initialize_ui()
+        # Create the widgets
+        self.devices_table = tk.Frame(self.parent, bd=1, relief="solid")
+    #     self.devices_table.pack(row=1, column=0, sticky=tk.NSEW)
+        self.devices_table.columnconfigure(0, weight=1)
+        self.devices_table.columnconfigure(1, weight=1)
+
+        self.device_label = tk.Label(self.devices_table, text="Devices")
+    #     self.device_label.pack(row=0, column=0, padx=5, pady=5)
+
+        self.status_label = tk.Label(self.devices_table, text="Scanning...", font=("Arial", 16))
+    #     self.status_label.pack(row=0, column=1, sticky="nsew", padx=5, pady=5)
+
+        # Start the device scanning process
+        self.scan_devices()
+
+    def stop(self):
+        self._stop_event.set()
+
+    def _scan_devices(self):
+        self.devices = []
+        subnet = '.'.join(socket.gethostbyname_ex(socket.gethostname())[2][0].split('.')[0:3]) + '.'
+        for i in range(1, 255):
+            if self._stop_event.is_set():
+                break
+
+            ip = subnet + str(i)
+            if ip != socket.gethostbyname(socket.gethostname()):
+                try:
+                    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    s.settimeout(self.timeout_value)
+                    s.connect((ip, 135))
+                    s.close()
+                    self.devices.append({'ip': ip})
+                except socket.error as e:
+                    if e.errno != errno.ECONNREFUSED:
+                        pass
+                except socket.timeout:
+                    pass
+
+        # Display the found devices
+        self.parent.update_devices()
+
+    def run(self):
+        self._scan_devices()
+
+    def initialize(self):
+        self.create_labels()
+        self.create_entry_fields()
+        self.create_add_button()
+
+        # Create the listbox for devices
+        self.create_listbox()
+
+        # Create the progress bar
+        self.progress_var = tk.DoubleVar()
+        self.progress_var.set(0)
+        self.progress_bar = ttk.Progressbar(self.parent, orient="horizontal", mode="indeterminate", variable=self.progress_var)
+        self.progress_bar.pack(row=2, column=0, pady=5)
+
+    def create_labels(self):
+        self.device_name_label = tk.Label(self.parent, text="Device Name")
+        self.device_name_label.pack(row=3, column=0, padx=5, pady=5)
+        self.device_ip_label = tk.Label(self.parent, text="Device IP")
+        self.device_ip_label.pack(row=4, column=0, padx=5, pady=5)
+
+    def create_entry_fields(self):
+        self.device_name_entry = tk.Entry(self.parent)
+        self.device_name_entry.pack(row=3, column=1, padx=5, pady=5)
+        self.device_ip_entry = tk.Entry(self.parent)
+        self.device_ip_entry.pack(row=4, column=1, padx=5, pady=5)
+        self.ip_entry_var = tk.StringVar()
+        self.ip_entry = tk.Entry(self.parent, textvariable=self.ip_entry_var)
+        self.ip_entry_var.set("Enter IP address...")
+        self.ip_entry.bind('<FocusIn>', self.on_entry_click)
+        self.ip_entry.bind('<FocusOut>', self.on_focusout)
+        self.ip_entry.pack(row=5, column=1, padx=5, pady=5)
+        self.port_entry = tk.Entry(self.parent)
+        self.port_entry.pack(row=6, column=1, padx=5, pady=5)
+        self.add_button = tk.Button(self.parent, text="Add", command=self.add_device)
+        self.add_button.pack(row=7, column=1, padx=5, pady=5)
+
+    def on_entry_click(self, event):
+        """function that gets called whenever entry is clicked"""
+        if self.ip_entry_var.get() == 'Enter IP address...':
+            self.ip_entry_var.set('')
+
+    def on_focusout(self, event):
+        """function that gets called whenever entry is clicked"""
+        if self.ip_entry_var.get() == '':
+            self.ip_entry_var.set('Enter IP address...')
+
+    def create_add_button(self):
+        self.add_button = tk.Button(self.parent, text="Add", command=self.add_device)
+        self.add_button.pack(row=5, column=1, padx=5, pady=5)
+
+    def add_device(self):
+        device_name = self.device_name_entry.get()
+        device_ip = self.device_ip_entry.get()
+        # do something with the device name and ip here
+
+    def create_listbox(self):
+        # create the devices table
+        self.devices_table = tk.Frame(self.parent, bd=1, relief="solid")
+        self.devices_table.pack(row=0, column=0, pady=5)
+
+        self.devices_table.columnconfigure(0, weight=1)
+        self.devices_table.columnconfigure(1, weight=1)
+        self.devices_table.columnconfigure(2, weight=1)
+
+        # create the listbox for devices
+        self.device_listbox = tk.Listbox(self.devices_table, height=10)
+        self.device_listbox.pack(row=1, column=0, sticky="nsew", padx=10, pady=10)
+
+
 
     def handle_error(self, title, message):
         self.after(0, messagebox.showerror, title, message)
 
-    def initialize_ui(self):
-        # Create the "Scan Devices" button
-        scan_button = tk.Button(self, text="Scan Devices", command=self.start_device_scan)
-        scan_button.pack(pady=5)
-
-        # Create the devices table
-        self.devices_table = tk.Frame(self, bd=1, relief="solid")
-        self.devices_table.pack(pady=5)
-
-        # Create the progress bar
-        self.progress_bar = ttk.Progressbar(self, orient="horizontal", mode="indeterminate")
-        self.progress_bar.pack(pady=5)
-
-    def start_device_scan(self):
-        # Start the device scanning process in a separate thread
-        scan_thread = Thread(target=self.scan_devices)
-        scan_thread.start()
 
     def scan_devices(self):
-        try:
-            # Display the progress bar and disable the scan button
-            self.progress_bar.start()
-            scan_button = self.winfo_children()[0]
-            scan_button.configure(state=tk.DISABLED)
+        self.progress_bar.start()
 
-            # Get the IP range to scan
-            ip_range = self.get_ip_range()
+        def _scan_devices():
 
-            # Scan the devices in the IP range
+            # Get all network interfaces and their IP addresses
+            interfaces = netifaces.interfaces()
+            ip_addresses = []
+            for interface in interfaces:
+                addresses = netifaces.ifaddresses(interface)
+                ip_addresses.extend([addr['addr'] for addr in addresses.get(netifaces.AF_INET, [])])
+
+            # Scan the devices for each IP address
             devices = []
             with ThreadPoolExecutor(max_workers=self.num_threads) as executor:
-                futures = [executor.submit(self.get_device_info, ip) for ip in ip_range]
+                futures = [executor.submit(self.get_device_info, ip) for ip in ip_addresses]
                 for future in futures:
                     device_info = future.result()
                     if device_info:
@@ -83,75 +185,67 @@ class DeviceDetector:
             # Store scanned devices in the instance variable
             self.devices = devices
 
+            # Update the devices table
+            self.update_devices_table(devices)
+
+            # Re-enable the scan button
+            scan_button.configure(state=tk.NORMAL)
+
+        try:
+            # Call the _scan_devices() function in a separate thread to avoid freezing the UI
+            scan_thread = Thread(target=_scan_devices)
+            scan_thread.start()
+
         except Exception as e:
             self.handle_error("Error", f"An error occurred while trying to scan devices: {e}")
 
         finally:
-            # Stop the progress bar and re-enable the scan button
+            # Stop the progress bar
             self.progress_bar.stop()
-            scan_button.configure(state=tk.NORMAL)
 
-    def get_devices(self):
-        # Implement the logic for fetching devices here
-        devices = []  # Replace with the actual list of devices
-        return devices
 
     def get_ip_range(self):
         try:
-            local_ip = None
-            for addrs in psutil.net_if_addrs().values():
-                for addr in addrs:
-                    if addr.family == socket.AF_INET:
-                        local_ip = addr.address
-                        netmask = addr.netmask
-                        break
-
-                if local_ip and netmask:
-                    break
-
-            if not local_ip or not netmask:
-                raise ValueError("Could not find a default gateway to determine the IP range.")
-
-            ip_interface = ipaddress.IPv4Interface(f"{local_ip}/{netmask}")
-            ip_network = ip_interface.network
-            ip_range = [str(ip) for ip in ip_network.hosts()]
-
+            ipv4_interfaces = [i for i in netifaces.interfaces() if netifaces.AF_INET in netifaces.ifaddresses(i)]
+            ip_net_pairs = []
+            for i in ipv4_interfaces:
+                try:
+                    address_info = netifaces.ifaddresses(i)[netifaces.AF_INET][0]
+                    ip_net_pairs.append((address_info['addr'], address_info.get('netmask')))
+                except KeyError:
+                    logger.warning(f"No netmask found for interface {i}. Skipping.")
+            ip_range = set()
+            for ip_net_pair in ip_net_pairs:
+                ip, netmask = ip_net_pair
+                if netmask:
+                    ip_interface = ipaddress.IPv4Interface(f"{ip}/{netmask}")
+                    ip_network = ip_interface.network
+                    ip_range.update([str(ip) for ip in ip_network.hosts()])
             return ip_range
-
         except Exception as e:
             logger.error(f"Error while trying to get IP range: {e}")
             raise
- 
+
+
     def update_devices_table(self, devices):
+        # Clear the previous content of the devices table
         for widget in self.devices_table.winfo_children():
             widget.destroy()
 
-        headers = ["Device Name", "IP Address", "MAC Address", "Device Type", "Software Version", "CPU Data", "Memory Data"]
-        for i, header in enumerate(headers):
-            label = tk.Label(self.devices_table, text=header, font=("Arial", 10, "bold"))
-            label.grid(row=0, column=i, padx=5, pady=5)
+        # Add the column headers
+        tk.Label(self.devices_table, text="IP Address").pack(row=0, column=0, padx=5, pady=5)
+        tk.Label(self.devices_table, text="Hostname").pack(row=0, column=1, padx=5, pady=5)
+        tk.Label(self.devices_table, text="MAC Address").pack(row=0, column=2, padx=5, pady=5)
 
-        for i, device in enumerate(devices, start=1):
-            device_name_label = tk.Label(self.devices_table, text=device.get("device_name", "Unknown"))
-            device_name_label.grid(row=i, column=0, padx=5, pady=5)
+        # Add the devices to the table
+        for index, device in enumerate(devices):
+            ip_address = device.get("ip_address")
+            hostname = device.get("hostname", "")
+            mac_address = device.get("mac_address", "")
 
-            device_ip_label = tk.Label(self.devices_table, text=device["ip"])
-            device_ip_label.grid(row=i, column=1, padx=5, pady=5)
-
-            device_mac_label = tk.Label(self.devices_table, text=device.get("mac", "Unknown"))
-            device_mac_label.grid(row=i, column=2, padx=5, pady=5)
-
-            device_type_label = tk.Label(self.devices_table, text=device.get("device_type", "Unknown"))
-            device_type_label.grid(row=i, column=3, padx=5, pady=5)
-
-            software_version_label = tk.Label(self.devices_table, text=device.get("software_version", "Unknown"))
-            software_version_label.grid(row=i, column=4, padx=5, pady=5)
-
-            cpu_data_label = tk.Label(self.devices_table, text=device.get("cpu_data", "Unknown"))
-            cpu_data_label.grid(row=i, column=5, padx=5, pady=5)
-
-            memory_data_label = tk.Label(self.devices_table, text=device.get("memory_data", "Unknown"))
-            memory_data_label.grid(row=i, column=6, padx=5, pady=5)
+            tk.Label(self.devices_table, text=ip_address).pack(row=index+1, column=0, padx=5, pady=5)
+            tk.Label(self.devices_table, text=hostname).pack(row=index+1, column=1, padx=5, pady=5)
+            tk.Label(self.devices_table, text=mac_address).pack(row=index+1, column=2, padx=5, pady=5)
 
     def get_device_info(self, ip):
         try:
@@ -160,9 +254,9 @@ class DeviceDetector:
             mac = get_mac_address(ip=ip)
 
             device_info = {
-                "ip": ip,
+                "ip_address": ip,
                 "hostname": hostname,
-                "mac": mac,
+                "mac_address": mac,
                 "device_name": "Unknown",
                 "device_type": "Unknown",
                 "software_version": "Unknown",
@@ -170,32 +264,6 @@ class DeviceDetector:
                 "memory_data": "Unknown"
             }
 
-            # Get additional device information (e.g., device name, device type, etc.) using requests or other libraries
-
-            # ... (code to fetch additional device information)
-
-            return device_info
-
-        except Exception as e:
-            logger.error(f"Error while trying to get device info for IP {ip}: {e}")
-            return None
-
-    def get_device_info(self, ip):
-        try:
-            # Get basic device information
-            hostname = socket.getfqdn(ip)
-            mac = get_mac_address(ip=ip)
-
-            device_info = {
-                "ip": ip,
-                "hostname": hostname,
-                "mac": mac,
-                "device_name": "Unknown",
-                "device_type": "Unknown",
-                "software_version": "Unknown",
-                "cpu_data": "Unknown",
-                "memory_data": "Unknown"
-            }
 
             # Get additional device information (e.g., device name, device type, etc.) using requests or other libraries
             try:
@@ -217,4 +285,3 @@ class DeviceDetector:
         except Exception as e:
             logger.error(f"Error while trying to get device info for IP {ip}: {e}")
             return None
-
--- gui.py
+++ gui.py
@@ -1,79 +1,89 @@
-import os
-import sys
 import tkinter as tk
-import tkinter.messagebox as messagebox
 from tkinter import ttk
-from tkinter.messagebox import showinfo
 
-from device_detector import DeviceDetector
-from PIL import Image, ImageTk
-from tkintertable import TableCanvas, TableModel
+from device_detection import CustomDeviceDetector
 
-from color_swatch import color_swatch
-from device_clustering import DeviceClustering
+class Ui_IoTShield(tk.Tk):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.title("AI Cyber IoT Shield")
+        self.geometry("1080x780")
+        self.config(bg="purple")
 
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-
-class Ui_IoTShield:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("AI Cyber IoT Shield")
-        self.loading_popup = None
-        timeout_value = 2  # You can set this value according to your needs
-        self.device_detector = DeviceDetector(num_threads=timeout_value)
-
-        self.root.title("AI IoT Shield")
-        self.root.geometry("840x780")
-
-        self.root.config(bg="purple")
-        self.main_frame = tk.Frame(self.root)
-        self.main_frame.pack(fill=tk.BOTH, expand=True)
-        # self.create_buttons()  # Remove this line
-        self.create_listbox()
-        self.create_labels()
-        self.create_entry_fields()
+        # Create the widgets for the UI
         self.create_widgets()
 
-        self.frame_1 = tk.Frame(self.root, bg="white")
-        self.frame_1.pack(side="left", fill="both",
-                          expand=True, padx=10, pady=10)
-        self.frame_2 = tk.Frame(self.root, bg="white")
-        self.frame_2.pack(side="left", fill="both",
-                          expand=True, padx=10, pady=10)
+        # Create a frame for the main content
+        self.main_frame = tk.Frame(self)
+        self.main_frame.pack(fill="both", expand=True)
 
-        self.frame_3 = tk.Frame(self.root, bg="white")
-        self.frame_3.pack(side="bottom", fill="both",
-                          expand=True, padx=10, pady=10)
+        # Create the IP address entry widget and add it to the main frame using pack
+        self.ip_entry = tk.Entry(self.main_frame)
+        self.ip_entry.pack(side=tk.TOP, padx=10, pady=10)
 
-    # Remove the entire create_buttons method
+        # Create a frame for the device detector
+        self.device_detector_frame = tk.Frame(self.main_frame)
+        self.device_detector_frame.pack(padx=10, pady=10)
 
-    def create_listbox(self):
+        # Create the devices detector object and display it
+        self.device_detector = CustomDeviceDetector(self.device_detector_frame, timeout_value=3)
+
+        # Create the device listbox and label
         self.device_listbox = tk.Listbox(self.main_frame)
-        self.device_listbox.grid(row=1, column=0, padx=10, pady=10)
+        self.device_listbox.pack(side=tk.TOP, fill="both", expand=True, padx=10, pady=10)
 
-    def create_labels(self):
         self.device_label = tk.Label(self.main_frame, text="Devices")
-        self.device_label.grid(row=2, column=0, padx=10, pady=10)
+        self.device_label.pack(side=tk.TOP, padx=10, pady=10)
 
-    def create_entry_fields(self):
-        self.device_entry = tk.Entry(self.main_frame)
-        self.device_entry.grid(row=3, column=0, padx=10, pady=10)
+        # Create the "Show Devices" button
+        self.show_devices_button = ttk.Button(self.main_frame, text="Show Devices", command=self.show_devices)
+        self.show_devices_button.pack(side=tk.TOP, pady=5)
 
+        def update_devices(self):
+        # Clear the device listbox
+            self.device_listbox.delete(0, tk.END)
+
+        # Get the list of devices and add their IP addresses to the listbox
+        devices = self.devices
+        for device in devices:
+            self.device_listbox.insert(tk.END, device['ip'])
+    
     def create_widgets(self):
-        self.show_devices_button = ttk.Button(
-            self.main_frame, text="Show Devices", command=self.show_devices)
-        self.show_devices_button.grid(row=4, column=0, padx=10, pady=10)
+        # Create a frame for the main content
+        self.main_frame = tk.Frame(self)
+        self.main_frame.pack(fill="both", expand=True)
+
+        # Create the IP address entry widget and add it to the main frame using pack
+        self.ip_entry = tk.Entry(self.main_frame)
+        self.ip_entry.pack(side=tk.TOP, padx=10, pady=10)
+
+        # Create a frame for the device detector
+        self.device_detector_frame = tk.Frame(self.main_frame)
+        self.device_detector_frame.pack(padx=10, pady=10)
+
+        # Create the devices detector object and display it
+        self.device_detector = CustomDeviceDetector(self.device_detector_frame, timeout_value=3)
+
+        # Create the device listbox and label
+        self.device_listbox = tk.Listbox(self.main_frame)
+        self.device_listbox.pack(side=tk.TOP, fill="both", expand=True, padx=10, pady=10)
+
+        self.device_label = tk.Label(self.main_frame, text="Devices")
+        self.device_label.pack(side=tk.TOP, padx=10, pady=10)
+
+        # Create the "Show Devices" button
+        self.show_devices_button = ttk.Button(self.main_frame, text="Show Devices", command=self.show_devices)
+        self.show_devices_button.pack(side=tk.TOP, pady=5)
 
     def show_devices(self):
-        devices = self.device_detector.some_other_method_name()  # Update this line with the correct method name
-        self.device_detector.update_devices_table(devices)
+        # Clear the device listbox
+        self.device_listbox.delete(0, tk.END)
+
+        # Get the list of devices and add their IP addresses to the listbox
+        devices = self.device_detector.devices
         for device in devices:
-            self.device_listbox.insert(tk.END, device)
+            self.device_listbox.insert(tk.END, device['ip'])
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = Ui_IoTShield(root)
+    root = Ui_IoTShield(None)
     root.mainloop()
