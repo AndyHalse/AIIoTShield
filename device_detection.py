58ccf1a674a4591504d175d01244db75106bf253
AndyHalse <128831696+AndyHalse@users.noreply.github.com>
2023-05-06 00:54:10 +0200

    Add files via upload
    Updated files with the correct scanning code, however, main.py throws an error!

--- device_detection.py
+++ device_detection.py
@@ -11,7 +11,7 @@ import nmap
 from nmap import PortScanner
 from PIL import Image, ImageTk
 from sklearn.cluster import KMeans
-
+import paramiko
 
 class NetworkDevice:
     def __init__(self, ip_address, mac_address, vendor_name, device_type, os, os_version, open_ports, subnet_info, dhcp_info, network_info, security_info):
@@ -21,175 +21,129 @@ class NetworkDevice:
         self.device_type = device_type
         self.os = os
         self.os_version = os_version
-        self.open_ports = open_ports 
+        self.open_ports = open_ports
         self.subnet_info = subnet_info
         self.dhcp_info = dhcp_info
         self.network_info = network_info
         self.security_info = security_info
 
-        self.subnet_mask = subnet_mask
-        self.default_gateway = default_gateway
-        self.dhcp_server = dhcp_server
-        self.nic = nic
-        self.network_topology = network_topology
-        self.routing_protocol = routing_protocol
-        self.switches = switches
-        self.routers = routers
-        self.firewall = firewall
-        self.ports = ports
-        self.vlans = vlans
-        self.qos = qos
-        self.nat = nat
-        self.vpn = vpn
-        self.remote_access_protocol = remote_access_protocol
-        self.authentication_and_authorization_mechanisms = authentication_and_authorization_mechanisms
-        self.network_monitoring_and_management_tools = network_monitoring_and_management_tools
-        self.bandwidth_utilization = bandwidth_utilization
-        self.packet_loss = packet_loss
-        self.latency = latency
-        self.network_security_protocols = network_security_protocols
-        self.dns_resolution = dns_resolution
-        self.tcp_ip = tcp_ip
-        self.udp = udp
-        self.icmp = icmp
-        self.ipsec = ipsec
-        self.ssl_tls_encryption = ssl_tls_encryption
-        self.snmp = snmp
-        self.ntp = ntp
-        self.dhcp_options = dhcp_options
+        self.subnet_mask = None
+        self.default_gateway = None
+        self.dhcp_server = None
+        self.nic = None
+        self.network_topology = None
+        self.routing_protocol = None
+        self.switches = []
+        self.routers = []
+        self.firewall = None
+        self.ports = []
+        self.vlans = []
+        self.qos = None
+        self.nat = None
+        self.vpn = None
+        self.remote_access_protocol = None
+        self.authentication_and_authorization_mechanisms = []
+        self.network_monitoring_and_management_tools = []
+        self.bandwidth_utilization = None
+        self.packet_loss = None
+        self.latency = None
+        self.network_security_protocols = []
+        self.dns_resolution = None
+        self.tcp_ip = None
+        self.udp = None
+        self.icmp = None
+        self.ipsec = None
+        self.ssl_tls_encryption = None
+        self.snmp = None
+        self.ntp = None
+        self.dhcp_options = None
         self.devices = []
 
-    def scan_network(self):
-        # retrieve IP address of current host
-        current_host = socket.gethostname()
-        host_ip = socket.gethostbyname(current_host)
+def scan_network(self, destination):
+    # retrieve IP address of current host
+    current_host = socket.gethostname()
+    host_ip = socket.gethostbyname(current_host)
 
-        # extract network address and netmask
-        network_address = str(ipaddress.ip_network(host_ip + '/24', False).network_address)
-        netmask = str(ipaddress.ip_network(host_ip + '/24', False).prefixlen)
+    # extract network address and netmask
+    network_address = str(ipaddress.ip_network(
+        host_ip + '/24', False).network_address)
+    netmask = str(ipaddress.ip_network(
+        host_ip + '/24', False).prefixlen)
 
-        # reset devices list before scanning network
-        self.devices = []
+    # reset devices list before scanning network
+    self.devices = []
 
-        # retrieve subnet mask and default gateway
-        route_info = subprocess.check_output("route -n get default".split()).decode().strip()
-        subnet_mask = ''
-        default_gateway = ''
-        for line in route_info.split('\n'):
-            if 'netmask' in line:
-                subnet_mask = line.split()[-1]
-            elif 'gateway' in line:
-                default_gateway = line.split()[-1]
+    # retrieve subnet mask and default gateway
+    route_info = subprocess.check_output(
+        "netstat -nr | grep '^default'".split()).decode().strip()
+    subnet_mask = ''
+    default_gateway = ''
+    for line in route_info.split('\n'):
+        if 'netmask' in line:
+            subnet_mask = line.split()[-2]
+        elif 'default' in line:
+            default_gateway = line.split()[-2]
 
-        # retrieve DNS servers and DHCP server
-        dns_servers = netifaces.gateways()['default'][2]
-        dhcp_server = subprocess.check_output("ipconfig getoption en0 server_identifier".split()).decode().strip()
+    self.subnet_mask = subnet_mask
+    self.default_gateway = default_gateway
 
-        # get NIC information
-        nic = subprocess.check_output("networksetup -listallhardwareports".split()).decode().strip()
+    # retrieve DNS servers and DHCP server
+    dns_servers = netifaces.gateways()['default'][2]
+    dhcp_server = subprocess.check_output(
+        "networksetup -getinfo Ethernet | grep 'Server Identifier'".split()).decode().strip()
+    dhcp_server = dhcp_server.split(': ')[-1]
 
-        # get network topology
-        network_topology = subprocess.check
+    self.dns_servers = dns_servers
+    self.dhcp_server = dhcp_server
 
-    @staticmethod
-    def clean_ua(user_agent):
-        try:
-            # code to clean user agent
-            pass
-        except Exception as e:
-            print(f"Error cleaning user agent: {e}")
-            return ''
+    # get NIC information
+    nic_info_lines = subprocess.check_output(
+        "networksetup -listallhardwareports".split()).decode().strip().split('\n')
+    nic_info = []
+    for line in nic_info_lines[1:]:
+        if 'Hardware Port' in line:
+            # move initialization of nic_dict here
+            nic_dict = {'Hardware Port': line.split(':')[1].strip()}
+            for next_line in nic_info_lines[nic_info_lines.index(line)+1:]:
+                if 'Ethernet Address' in next_line:
+                    # create a new dictionary for each network interface
+                    nic_dict['Ethernet Address'] = next_line.split(':')[-1].strip()
+                    # append the dictionary to the list
+                    nic_info.append(nic_dict)
+                    break
 
-    def __str__(self):
-        return f'{self.ip_address} {self.mac_address} ({self.vendor_name})'
+    self.nic = nic_info
 
+    # use nmap to scan for open ports
+    nm = nmap.PortScanner()
+    arguments = f"-sS -sV -O {network_address}/{netmask}"
+    scan_results = nm.scan(arguments=arguments)
+    for ip, results in scan_results['scan'].items():
+        if results['status']['state'] == 'up':
+            open_ports = []
+            for port, data in results['tcp'].items():
+                if data['state'] == 'open':
+                    open_ports.append(port)
 
-    def __repr__(self):
-        return str(self)
+            # check the network topology
+            topology_output = os.popen(f"traceroute {destination}").read()
+            hops = []
+            for line in topology_output.split('\n'):
+                if 'traceroute to' in line:
+                    continue
+                if '  1 ' in line:
+                    hops.append(line.split()[1])
+                elif '\t2 ' in line:
+                    hops.append(line.split()[1])
+                elif '\t3 ' in line:
+                    hops.append(line.split()[1])
 
-    def __eq__(self, other):
-        return self.mac_address == other.mac_address
+            device = NetworkDevice(
+                ip, results['addresses']['mac'], results.get('vendor', ''),
+                subnet_mask, default_gateway, dns_servers, open_ports,
+                dhcp_server, nic_info[0]['Hardware Port'],
+                nic_info[0]['Ethernet Address'], netmask, hops)
+            self.devices.append(device)
+    return self.devices
 
-    def __hash__(self):
-        return hash(self.mac_address)
-
-    def to_dict(self):
-        return {
-            'ip_address': self.ip_address,
-            'mac_address': self.mac_address,
-            'vendor_name': self.vendor_name,
-            'device_type': self.device_type,
-            'os': self.os,
-            'os_version': self.os_version,
-            'open_ports': self.open_ports,
-            'dns': self.dns
-        }
-
-    def __str__(self):
-        return f'{self.ip_address} {self.mac_address} ({self.vendor_name})'
-
-
-    def __repr__(self):
-        return str(self)
-
-    def __eq__(self, other):
-        return self.mac_address == other.mac_address
-
-    def __hash__(self):
-        return hash(self.mac_address)
-
-    def to_dict(self):
-        return {
-            'ip_address': self.ip_address,
-            'mac_address': self.mac_address,
-            'vendor_name': self.vendor_name,
-            'device_type': self.device_type,
-            'os': self.os,
-            'os_version': self.os_version,
-            'open_ports': self.open_ports,
-            'dns': self.dns
-        }
-
-    @classmethod
-    def from_dict(cls, data):
-        return cls(
-            data['ip_address'], data['mac_address'], data['vendor_name'],
-            data['device_type'], data['os'], data['os_version'], data['open_ports'], data['dns']
-        )
-
-
-    def cluster_devices(self):
-        ips = [ipaddress.ip_address(device.ip_address) for device in self.devices]
-        kmeans = KMeans(n_clusters=min(5, len(ips)), random_state=0).fit([[int(ip)] for ip in ips])
-        cluster_labels = kmeans.labels_
-
-        for i, device in enumerate(self.devices):
-            device.model = f"Cluster {cluster_labels[i]+1}"
-
-    def get_detected_devices(self):
-        return self.devices
-
-    def save_devices_csv(self):
-        with open(self.csv_file, 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(["IP Address", "Hostname"])
-            writer.writerows(self.devices)
-
-    # Function to generate secure logs
-    def generate_secure_log(self, log_entry):
-        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        log_entry = f"{timestamp} - {log_entry}\n"
-
-        with open(self.csv_file, "a") as f:
-            f.write(log_entry)
-
-        log_hash = hashlib.sha256(log_entry.encode("utf-8")).hexdigest()
-        with open(self.csv_file + ".hash", "a") as f:
-            f.write(log_hash + "\n")
-
-    # Function to securely log activities
-    def log_activity(self, activity):
-        log_entry = f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - {activity}\n"
-        log_entry_hash = hashlib.sha256(log_entry.encode("utf-8")).hexdigest()
-        with open(self.csv_file, "a") as log_file:
-            log_file.write(f"{log_entry}{log_entry_hash}\n")
+           
